ba8459677997604b66de20f42249854431bdfb1b
Max <161768135+Picoodi@users.noreply.github.com>
2024-04-06 21:47:31 +0200

    Update and rename Palindrome Number to PalindromeNumber.py

---
+++ PalindromeNumber.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, number: int) -> bool:
+
+        #If the integer is negativ it cant be a palindrom
+        if number < 0:
+            return False
+        else:
+            #we create a string of the integer and read every element into a list
+            strint = str(number)
+            listofint = [str(digit) for digit in strint]
+            #we reverse the list with the .reverse() function
+            listofint.reverse()
+            #create a new string with the join() function and look if the old and
+            #new string are the same which returns true and if not it returns false
+            newstrint = ''.join(listofint)
+            if newstrint == strint:
+                return True
+            else:
+                return False
+            
